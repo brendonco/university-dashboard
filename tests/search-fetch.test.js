ef8bfb9373fd4433d6817386e227d4848b330921
Brendon Co <brendonco@hotmail.com>
2021-02-05 23:30:05 +0800

    Cleanup

--- tests/search-fetch.test.js
+++ tests/search-fetch.test.js
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
 
-import { act, fireEvent, screen, waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
@@ -40,11 +40,7 @@ afterAll(() => server.close());
 test('loads and displays greeting', async () => {
     setup();
 
-    // fireEvent.change(input, { target: { value: 'jakarta' } });
     await waitFor(() => {
         expect(screen.getAllByRole('listitem')).toHaveLength(3);
     });
-
-    // expect(await screen.findByText('Politeknik Negeri Jakarta')).toBeInTheDocument();
-    // expect(screen.getByRole('heading')).toHaveTextContent('Politeknik Negeri Jakarta');
 });
