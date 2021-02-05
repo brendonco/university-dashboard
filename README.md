6c9c49a3cf3f72bb3afd7bd1368b856ba9328307
Brendon Co <brendonco@hotmail.com>
2021-02-05 12:30:48 +0800

    Enable cors and update readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# University Dashboard
+
+## Components
+- NavBar - navigation using next router.
+- Card - Display dynamic card layout.
+- Layout - Main layout of the component.
+- ErrorBoundary - Catch nasty errors and dump into console. Good to store in log server.
+- TraceSpinner - show spinner while fetching data.
+
+## Pages
+- Search - For searching universities
+- Newsletter - For dumping user input into a json file
--- pages/newsletter/index.js
+++ pages/newsletter/index.js
@@ -1,5 +1,21 @@
 import Layout from '../../components/Layout';
 
-const Newsletter = () => <Layout>Newsletter</Layout>;
+const downloadTxtFile = () => {
+    const element = document.createElement('a');
+    const file = new Blob([document.getElementById('input').value], {
+        type: 'application/json;charset=utf-8'
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = 'users.json';
+    document.body.appendChild(element);
+    element.click();
+};
+
+const Newsletter = () => (
+    <Layout>
+        <input id="input" />
+        <button onClick={downloadTxtFile}>Download</button>
+    </Layout>
+);
 
 export default Newsletter;
